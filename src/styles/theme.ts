b05a573f73e72eed7f313907a7f5227736c0a84d
Gregório Magno Neto <gregorio.neto@maida.health>
2023-07-03 13:39:12 -0300

    style: modificando theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,9 @@
 export default {
-    colors:{
-        white: '#FFFFFF'
-    }
-}
+  colors: {
+    white: "#FFFFFF",
+    grey: "#677294",
+    light_grey: "#ebeef5",
+    black: "#000000",
+    light_blue: " #0dcaf0",
+  },
+};
