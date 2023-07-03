d4b06c1b05d4aa993580257a4957f9debc6c6378
Gregório Magno Neto <gregorio.neto@maida.health>
2023-07-03 13:40:41 -0300

    feat: adicionando assets e constantes no projeto

---
+++ src/constants/contacts.ts
@@ -0,0 +1,7 @@
+export const CONTACT_EMAIL ='contato@leadster.com.br';
+
+export const CONTACT_PHONE ='(42) 998828-9851'
+
+export const INSTAGRAM = 'https://www.instagram.com/leadster.com.br/'
+export const LINKEDIN = 'https://www.linkedin.com/company/getleadster/'
+export const FACEBOOK = 'https://www.facebook.com/leadsterplatform'
