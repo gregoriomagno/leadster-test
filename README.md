e0b926f9d75edb94208583d1aac1cd639fe87905
Gregório Magno Neto <gregorio.neto@maida.health>
2023-07-04 22:38:44 -0300

    feat: adicionando overlay no card de videos

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+
+icons referências:
+
+<a href="https://iconscout.com/icons/play" target="_blank">Free Play Icon</a> by <a href="https://iconscout.com/contributors/alexandru-stoica">Alexandru Stoica</a> on <a href="https://iconscout.com">IconScout</a>
--- src/components/atoms/CardVideo/index.tsx
+++ src/components/atoms/CardVideo/index.tsx
@@ -2,15 +2,21 @@ import React from "react";
 import Image from "next/image";
 
 import * as S from "./styled";
+import { ReactSVG } from "react-svg";
 const CardVideo = () => {
   return (
     <S.ContainerCard>
-      <Image
+      <S.ImageCard>
+      <img
         src={"/assets/thumbnail.png"}
         alt="thumbnail"
         width={363}
         height={204}
       />
+      <S.OverlayCard className="overlay">
+      <ReactSVG src={"/assets/play.svg"} />
+      </S.OverlayCard>
+      </S.ImageCard>
       <S.WrapperTitleCard>
         <h2>Como aumentar sua Geração de Leads feat. Traktos</h2>
       </S.WrapperTitleCard>
